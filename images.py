dc2b15cf985046164d904a47b4b85e55a2b637d2
Andrew Wentzel <tehwentzel@gmail.com>
2019-06-07 23:10:44 -0500

    basic code for local binary pattern histograms and combining color space histograms. gets lis 82ish percent accuracy

--- images.py
+++ images.py
@@ -4,6 +4,7 @@ import glob
 from copy import copy
 import re
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 classes = ['Electron-Mix','Transmission',
                                'ReporterANDImmunoHistochem', 'Fluorescence-Mix',
@@ -35,36 +36,91 @@ def get_unsegmented_image_files(root = "data\samples*\*.bmp"):
         files.remove(file)
     return files
 
-def get_histogram(file, bins = 5, n_channels = 3, color_space = cv2.COLOR_BGR2YCrCb):
+def get_histogram(file, bins = 5, color_space = cv2.COLOR_BGR2YCrCb):
     image = cv2.imread(file)
     image = cv2.cvtColor(image, color_space)
+    if len(image.shape) > 2:
+        n_channels = image.shape[2]
+    else:
+        n_channels = 1
     hists = []
-    scale = lambda x: (x - x.min())/(x.max() - x.min())
+#    scale = lambda x: (x - x.min())/(x.max() - x.min())
     for channel in range(n_channels):
+        if n_channels > 1:
+            img_slice = image[:,:,channel]
+        else:
+            img_slice = image
 #        image[:,:,channel] = scale(image[:,:,channel])
-        color_histogram = np.histogram(image[:,:,channel], bins = bins, density = True)[0]
+        color_histogram = np.histogram(img_slice, bins = bins, density = True)[0]
         hists.append(color_histogram.astype('float32'))
     return(hists)
 
-def get_correlations(file_dict = None, method = cv2.HISTCMP_CHISQR, color_space = cv2.COLOR_BGR2YCrCb):
+def get_histograms(file_dict = None,
+                     color_space = cv2.COLOR_BGR2YCrCb, 
+                     bins = 15):
     if file_dict is None:
         file_dict = get_image_files()
     hist_distance_dict = {}
     for c in file_dict.keys():
         image_files  = file_dict[c]
-        correlation = np.zeros((len(image_files),3))
+        correlation = []
         position = 0
         for image_file in image_files:
-            y,r,g = get_histogram(image_file, color_space = color_space)
-            correlation[position, 0] = cv2.compareHist(r,g, method)
-            correlation[position, 1] = cv2.compareHist(y,r, method)
-            correlation[position, 2] = cv2.compareHist(y,g, method)
+            all_hists = []
+            hists = get_histogram(image_file, color_space = color_space, bins = bins)
+            for histogram in hists:
+                all_hists.extend(histogram)
             position += 1
-        hist_distance_dict[c] = correlation
+            correlation.append(np.hstack(all_hists).ravel())
+        print(np.vstack(correlation).shape)
+        hist_distance_dict[c] = np.vstack(correlation)
     return hist_distance_dict
 
-d = get_correlations(file_dict = file_dict, 
-#                     color_space = cv2.COLOR_BGR2RGB, 
-                     method = cv2.HISTCMP_BHATTACHARYYA)
-for c, v in d.items():
-    print(classes[c], v.mean(axis = 0))
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from skimage.feature import local_binary_pattern
+#file_dict = get_image_files()
+#features = []
+#for color_space in [cv2.COLOR_BGR2GRAY, 
+#                    cv2.COLOR_BGR2RGB, 
+#                    cv2.COLOR_BGR2HSV
+#                    ]:
+#    color_hists = get_histograms(file_dict = file_dict, 
+#                                 color_space = color_space)
+#    color_matrix = np.vstack([v for v in color_hists.values()])
+#    features.append(color_matrix)
+#class_labels = np.hstack([c*np.ones((v.shape[0],)) for c,v in color_hists.items()])
+bin_size = 15
+n_files = np.sum([len(fileset) for fileset in file_dict.values()])
+textures = np.zeros((n_files, bin_size))
+t_position = 0
+for c in range(max(file_dict.keys())):
+    files = file_dict[c]
+    matrix = np.zeros((len(files), bin_size))
+    position = 0
+    for file in files:
+        img = cv2.imread(file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        lbp = local_binary_pattern(img, 6, 2, method = 'ror')
+        histogram = np.histogram(lbp, density = True, bins = bin_size)[0]
+        matrix[position, :] = histogram
+        position += 0
+    end = t_position + len(files)
+    textures[t_position:end, :] = matrix
+    t_position = end
+x = np.hstack(features)
+x = np.hstack([x, textures])
+
+x_train, x_test, y_train, y_test = train_test_split(x, class_labels, stratify = class_labels)
+#pca = PCA(n_components = None)
+#x_train = pca.fit_transform(x_train)
+#x_test = pca.transform(x_test)
+
+model = RandomForestClassifier(n_estimators = 60,
+                               max_depth = 25,
+                               min_samples_split = 2,
+                               random_state = 0)
+model.fit(x_train,y_train)
+print(model.score(x_test, y_test))
+print(model.feature_importances_)
